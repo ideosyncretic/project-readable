81983eebf11699479514c6013efe8d76bdf38a63
Angel Coco Chen <angel@nuisance.co>
2018-01-20 15:07:52 +0800

    Scaffold folders and create redux store
    - install react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
+import { createStore, applyMiddleware } from "redux"
+import "./index.css"
+import App from "./App"
+import registerServiceWorker from "./registerServiceWorker"
+import reduxThunk from "redux-thunk"
+import reducers from "./reducers"
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
+const store = createStoreWithMiddleware(reducers)
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+)
+registerServiceWorker()
