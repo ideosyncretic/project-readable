7292b66a9936f58a239e8bbc23b77fd268804e23
Angel Coco Chen <angel@nuisance.co>
2018-02-14 09:39:48 +0800

    Fetch and display categories in EditPostForm
    - add Select type to InputField

--- client/src/modules/post/components/EditPostForm.js
+++ client/src/modules/post/components/EditPostForm.js
@@ -3,31 +3,42 @@ import { Field } from 'redux-form'
 import InputField from './InputField'
 import { Box, ButtonOutline } from 'rebass'
 
-const EditPostForm = props => (
-  <Box
-    is="form"
-    onSubmit={props.handleSubmit(mockSubmit)}
-    w={[1, 3 / 4, 5 / 6]}
-    p={4}
-  >
-    <Field
-      name="title"
-      component={InputField}
-      label="Title"
-      placeholder="Title here"
-      type="text"
-    />
-    <Field
-      name="body"
-      component={InputField}
-      label="Body"
-      placeholder="Write something interesting!"
-      type="textarea"
-    />
-    <ButtonOutline type="submit">Post</ButtonOutline>
-  </Box>
-)
+const EditPostForm = props => {
+  const { categories } = props
+  return (
+    <Box
+      is="form"
+      onSubmit={props.handleSubmit(mockSubmit)}
+      w={[1, 3 / 4, 5 / 6]}
+      p={4}
+    >
+      <Field
+        name="title"
+        component={InputField}
+        label="Title"
+        placeholder="Title here"
+        type="text"
+      />
+      <Field
+        name="body"
+        component={InputField}
+        label="Body"
+        placeholder="Write something interesting!"
+        type="textarea"
+      />
+      <Field
+        name="category"
+        component={InputField}
+        label="Category"
+        placeholder="Write something interesting!"
+        type="select"
+        options={categories}
+      />
+      <ButtonOutline type="submit">Post</ButtonOutline>
+    </Box>
+  )
+}
 
 export default EditPostForm
 
-const mockSubmit = () => console.log('submitted!')
+const mockSubmit = params => console.log(params)
--- client/src/modules/post/components/InputField.js
+++ client/src/modules/post/components/InputField.js
@@ -1,23 +1,43 @@
 import React from 'react'
-import { Box, Input, Label, Textarea } from 'rebass'
+import { Box, Input, Label, Select, Textarea } from 'rebass'
 
-const InputField = props => {
-  const { input, placeholder, label, type } = props
-  return (
-    <Box pb={3}>
-      <Label htmlFor={input.name}>{label}</Label>
-      {type === 'textarea' ? (
-        <Textarea
-          rows={4}
-          {...input}
-          id={input.name}
-          placeholder={placeholder}
-        />
-      ) : (
-        <Input {...input} id={input.name} placeholder={placeholder} />
-      )}
-    </Box>
-  )
+const InputField = ({ input, placeholder, label, type, options }) => {
+  switch (type) {
+    case 'textarea':
+      return (
+        <Box pb={3}>
+          <Label htmlFor={input.name}>{label}</Label>
+          <Textarea
+            rows={6}
+            {...input}
+            id={input.name}
+            placeholder={placeholder}
+          />
+        </Box>
+      )
+    case 'text':
+      return (
+        <Box pb={3}>
+          <Label htmlFor={input.name}>{label}</Label>
+          <Input {...input} id={input.name} placeholder={placeholder} />
+        </Box>
+      )
+    case 'select':
+      return (
+        <Box pb={3}>
+          <Label htmlFor={input.name}>{label}</Label>
+          <Select pb={3}>
+            {options.map(option => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      )
+    default:
+      return null
+  }
 }
 
 export default InputField
